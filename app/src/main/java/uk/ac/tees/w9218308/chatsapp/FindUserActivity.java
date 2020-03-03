40ec6d0486ae7ef4e0b4423fb22bc55d950c4201
Varun Vasudevan <W9218308@live.tees.ac.uk>
2020-03-03 17:08:43 +0000

    Added Contacts and recycler view to display it

--- app/src/main/java/uk/ac/tees/w9218308/chatsapp/FindUserActivity.java
+++ app/src/main/java/uk/ac/tees/w9218308/chatsapp/FindUserActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -23,15 +25,28 @@ public class FindUserActivity extends AppCompatActivity {
 
         userList = new ArrayList<>();
         initializeRecyclerView();
+        getContactList();
     }
 
     private void initializeRecyclerView() {
         mUserList = findViewById(R.id.userList);
         mUserList.setNestedScrollingEnabled(false);
         mUserList.setHasFixedSize(false);
-        mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL,false);
+        mUserListLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
         mUserList.setLayoutManager(mUserListLayoutManager);
         mUserListAdapter = new UserListAdapter(userList);
         mUserList.setAdapter(mUserListAdapter);
+    }
+
+    private void getContactList() {
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        while (phones.moveToNext()) {
+            String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phone = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            UserObject mContact = new UserObject(name, phone);
+            userList.add(mContact);
+            mUserListAdapter.notifyDataSetChanged();
+        }
     }
 }
