5a358b86428151f7dd442d2fadf1589b783fdec3
Varun Vasudevan <W9218308@live.tees.ac.uk>
2020-03-16 00:10:21 +0000

    displaying media on chat

--- app/src/main/java/uk/ac/tees/w9218308/chatsapp/Chat/MessageAdapter.java
+++ app/src/main/java/uk/ac/tees/w9218308/chatsapp/Chat/MessageAdapter.java
@@ -3,11 +3,14 @@ package uk.ac.tees.w9218308.chatsapp.Chat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.stfalcon.frescoimageviewer.ImageViewer;
 
 import java.util.ArrayList;
 
@@ -33,9 +36,21 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MessageViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final MessageViewHolder holder, final int position) {
         holder.mMessage.setText(messageList.get(position).getMessage());
         holder.mSender.setText(messageList.get(position).getSenderId());
+
+        if(messageList.get(holder.getAdapterPosition()).getMediaUrlList().isEmpty())
+            holder.mViewMedia.setVisibility(View.GONE);
+
+        holder.mViewMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ImageViewer.Builder(v.getContext(), messageList.get(holder.getAdapterPosition()).getMediaUrlList())
+                        .setStartPosition(0)
+                        .show();
+            }
+        });
     }
 
     @Override
@@ -46,14 +61,15 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
     public class MessageViewHolder extends RecyclerView.ViewHolder {
         TextView mMessage, mSender;
-
+        Button mViewMedia;
         LinearLayout mLayout;
 
         MessageViewHolder(View view) {
             super(view);
             mLayout = view.findViewById(R.id.layout);
-            mMessage = view.findViewById(R.id.messageList);
+            mMessage = view.findViewById(R.id.message);
             mSender = view.findViewById(R.id.sender);
+            mViewMedia = view.findViewById(R.id.viewMedia);
         }
     }
 }
