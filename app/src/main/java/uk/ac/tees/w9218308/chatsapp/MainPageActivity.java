6196c70976c4944abbd694a7c9a41f49ee43a394
Varun Vasudevan <W9218308@live.tees.ac.uk>
2020-04-18 13:56:21 +0100

    added profile username and status code.

--- app/src/main/java/uk/ac/tees/w9218308/chatsapp/MainPageActivity.java
+++ app/src/main/java/uk/ac/tees/w9218308/chatsapp/MainPageActivity.java
@@ -36,39 +36,36 @@ import uk.ac.tees.w9218308.chatsapp.Utils.SendNotification;
 
 public class MainPageActivity extends AppCompatActivity {
 
-    private RecyclerView mChatList;
-    private RecyclerView.Adapter mChatListAdapter;
-    private RecyclerView.LayoutManager mChatListLayoutManager;
+//    private RecyclerView mChatList;
+//    private RecyclerView.Adapter mChatListAdapter;
+//    private RecyclerView.LayoutManager mChatListLayoutManager;
     private Toolbar mToolbar;
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
     private TabsAccessAdapter mTabsAccessAdapter;
 
-    ArrayList<ChatObject> chatList;
+//    ArrayList<ChatObject> chatList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-        OneSignal.startInit(this).init();
-        OneSignal.setSubscription(true);
-        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
-            @Override
-            public void idsAvailable(String userId, String registrationId) {
-                FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("notificationKey").setValue(userId);
-            }
-        });
-        OneSignal.setInFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification);
-
-        Fresco.initialize(this);
+//        OneSignal.startInit(this).init();
+//        OneSignal.setSubscription(true);
+//        OneSignal.idsAvailable(new OneSignal.IdsAvailableHandler() {
+//            @Override
+//            public void idsAvailable(String userId, String registrationId) {
+//                FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("notificationKey").setValue(userId);
+//            }
+//        });
+//        OneSignal.setInFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification);
+//
+//        Fresco.initialize(this);
 
         mToolbar = findViewById(R.id.mainToolBar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("ChatsApp");
-
-//        Button mFindUser = findViewById(R.id.findUser);
-//        Button mLogout = findViewById(R.id.logout);
 
         mViewPager = findViewById(R.id.mainTabPager);
         mTabsAccessAdapter = new TabsAccessAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
@@ -77,115 +74,95 @@ public class MainPageActivity extends AppCompatActivity {
         mTabLayout = findViewById(R.id.mainTabs);
         mTabLayout.setupWithViewPager(mViewPager);
 
-//        mFindUser.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(getApplicationContext(), FindUserActivity.class));
-//            }
-//        });
-//
-//        mLogout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                OneSignal.setSubscription(false);
-//                FirebaseAuth.getInstance().signOut();
-//                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                startActivity(intent);
-//                finish();
-//                return;
-//            }
-//        });
-
-        getPermissions();
+//        getPermissions();
 //        initializeRecyclerView();
-        getUserChatList();
+//        getUserChatList();
     }
 
-    private void getUserChatList() {
-        DatabaseReference mUserChatDB = FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("chat");
+//    private void getUserChatList() {
+//        DatabaseReference mUserChatDB = FirebaseDatabase.getInstance().getReference().child("user").child(FirebaseAuth.getInstance().getUid()).child("chat");
+//
+//        mUserChatDB.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.exists()) {
+//                    for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+//                        ChatObject mChat = new ChatObject(childSnapshot.getKey());
+//                        boolean exists = false;
+//                        for (ChatObject mChatIterator : chatList) {
+//                            if (mChatIterator.getChatId().equals(mChat.getChatId()))
+//                                exists = true;
+//                        }
+//                        if (exists)
+//                            continue;
+//                        chatList.add(mChat);
+//                        getChatData(mChat.getChatId());
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
-        mUserChatDB.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
-                        ChatObject mChat = new ChatObject(childSnapshot.getKey());
-                        boolean exists = false;
-                        for (ChatObject mChatIterator : chatList) {
-                            if (mChatIterator.getChatId().equals(mChat.getChatId()))
-                                exists = true;
-                        }
-                        if (exists)
-                            continue;
-                        chatList.add(mChat);
-                        getChatData(mChat.getChatId());
-                    }
-                }
-            }
+//    private void getChatData(String chatId) {
+//        DatabaseReference mChatDB = FirebaseDatabase.getInstance().getReference().child("chat").child(chatId).child("info");
+//        mChatDB.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                if (dataSnapshot.exists()) {
+//                    String chatId = "";
+//                    if (dataSnapshot.child("id").getValue() != null)
+//                        chatId = dataSnapshot.child("id").getValue().toString();
+//
+//                    for (DataSnapshot userSnapshot : dataSnapshot.child("users").getChildren()) {
+//                        for (ChatObject mChat : chatList) {
+//                            if (mChat.getChatId().equals(chatId)) {
+//                                UserObject mUser = new UserObject(userSnapshot.getKey());
+//                                mChat.addUserToArrayList(mUser);
+//                                getUserData(mUser);
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    private void getChatData(String chatId) {
-        DatabaseReference mChatDB = FirebaseDatabase.getInstance().getReference().child("chat").child(chatId).child("info");
-        mChatDB.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    String chatId = "";
-                    if (dataSnapshot.child("id").getValue() != null)
-                        chatId = dataSnapshot.child("id").getValue().toString();
-
-                    for (DataSnapshot userSnapshot : dataSnapshot.child("users").getChildren()) {
-                        for (ChatObject mChat : chatList) {
-                            if (mChat.getChatId().equals(chatId)) {
-                                UserObject mUser = new UserObject(userSnapshot.getKey());
-                                mChat.addUserToArrayList(mUser);
-                                getUserData(mUser);
-                            }
-                        }
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    private void getUserData(UserObject mUser) {
-        DatabaseReference mUserDb = FirebaseDatabase.getInstance().getReference().child("user").child(mUser.getUid());
-        mUserDb.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                UserObject mUser = new UserObject(dataSnapshot.getKey());
-
-                if (dataSnapshot.child("notificationKey").getValue() != null)
-                    mUser.setNotificationKey(dataSnapshot.child("notificationKey").getValue().toString());
-
-                for (ChatObject mChat : chatList) {
-                    for (UserObject mUserIterator : mChat.getUserObjectArrayList()) {
-                        if (mUserIterator.getUid().equals(mUser.getUid())) {
-                            mUserIterator.setNotificationKey(mUser.getNotificationKey());
-                        }
-                    }
-                }
-                mChatListAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
+//    private void getUserData(UserObject mUser) {
+//        DatabaseReference mUserDb = FirebaseDatabase.getInstance().getReference().child("user").child(mUser.getUid());
+//        mUserDb.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                UserObject mUser = new UserObject(dataSnapshot.getKey());
+//
+//                if (dataSnapshot.child("notificationKey").getValue() != null)
+//                    mUser.setNotificationKey(dataSnapshot.child("notificationKey").getValue().toString());
+//
+//                for (ChatObject mChat : chatList) {
+//                    for (UserObject mUserIterator : mChat.getUserObjectArrayList()) {
+//                        if (mUserIterator.getUid().equals(mUser.getUid())) {
+//                            mUserIterator.setNotificationKey(mUser.getNotificationKey());
+//                        }
+//                    }
+//                }
+////                mChatListAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
 //    private void initializeRecyclerView() {
 //        chatList = new ArrayList<>();
@@ -210,7 +187,10 @@ public class MainPageActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         super.onOptionsItemSelected(item);
         if (item.getItemId() == R.id.option_editProfile) {
-
+            Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
         }
         if (item.getItemId() == R.id.option_logout) {
             FirebaseAuth.getInstance().signOut();
@@ -222,9 +202,9 @@ public class MainPageActivity extends AppCompatActivity {
         return true;
     }
 
-    private void getPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
-        }
-    }
+//    private void getPermissions() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
+//        }
+//    }
 }
--- app/src/main/java/uk/ac/tees/w9218308/chatsapp/ProfileActivity.java
+++ app/src/main/java/uk/ac/tees/w9218308/chatsapp/ProfileActivity.java
@@ -1,14 +1,115 @@
 package uk.ac.tees.w9218308.chatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private EditText mUserName, mUserStatus;
+    private Button mUpdate;
+    private CircleImageView mProfilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        initialiseFields();
+
+        mUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateProfile();
+            }
+        });
+
+        retrieveUserInfo();
+    }
+
+    private void updateProfile() {
+
+        final String userName = mUserName.getText().toString();
+        final String pStatus = mUserStatus.getText().toString();
+
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(this, "Please enter User Name", Toast.LENGTH_SHORT).show();
+        } else {
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+            dbRef.child("user").child(currentUser.getUid()).child("name").setValue(userName);
+            dbRef.child("user").child(currentUser.getUid()).child("status").setValue(pStatus);
+        }
+        sendUserToMainActivity();
+    }
+
+    private void retrieveUserInfo() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        dbRef.child("user").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name") && (dataSnapshot.hasChild("image")))) {
+                    String uName = dataSnapshot.child("name").getValue().toString();
+                    String uStatus = dataSnapshot.child("status").getValue().toString();
+                    String uImage = dataSnapshot.child("image").getValue().toString();
+
+                    mUserName.setText(uName);
+                    mUserStatus.setText(uStatus);
+
+                } else if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name"))) {
+                    String uName = dataSnapshot.child("name").getValue().toString();
+                    String uStatus = dataSnapshot.child("status").getValue().toString();
+
+                    mUserName.setText(uName);
+                    mUserStatus.setText(uStatus);
+                }else {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void sendUserToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
+    private void initialiseFields() {
+        mUserName = findViewById(R.id.userName);
+        mUserStatus = findViewById(R.id.status);
+        mUpdate = findViewById(R.id.updateProfile);
+        mProfilePicture = findViewById(R.id.profileImage);
     }
 }
